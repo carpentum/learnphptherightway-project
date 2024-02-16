555fe7c4177bfb4534c459bb5e4876682be10145
Ramón <ramon.mosquera@gmail.com>
2024-02-16 10:06:05 +0100

    First commit

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+$root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+
+define('APP_PATH', $root . 'APP' . DIRECTORY_SEPARATOR);
+define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
+define('VIEWS_PATH',  $root . 'views' . DIRECTORY_SEPARATOR);
+
+require APP_PATH . 'App.php';
+
+$files = getTransactionFiles(FILES_PATH);
+
+var_dump($files);
